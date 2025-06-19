759a63af3512962bc4e175ba08d308a595ec8e00
smile9394 <ceo@shinjinbd.com>
2025-06-19 14:33:46 +0900

    user entity

--- src/blog/entities/blog.entity.ts
+++ src/blog/entities/blog.entity.ts
@@ -1,1 +1,17 @@
-export class Blog {}
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from '../../common/base.entity';
+
+@Entity()
+export class Blog extends BaseEntity {
+  @Column()
+  public title: string;
+
+  @Column()
+  public description: string;
+
+  @Column()
+  public price: number;
+
+  @Column()
+  public category: string;
+}
